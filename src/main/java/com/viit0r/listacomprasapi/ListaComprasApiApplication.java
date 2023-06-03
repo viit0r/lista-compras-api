e2dee342f0120eb75384e86807bdc9c1790b39a2
viit0r <vituuoliveira1@gmail.com>
2023-06-03 01:28:49 -0300

    Criação do projeto

---
+++ src/main/java/com/viit0r/listacomprasapi/ListaComprasApiApplication.java
@@ -0,0 +1,13 @@
+package com.viit0r.listacomprasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaComprasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaComprasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viit0r/listacomprasapi/ListaComprasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viit0r.listacomprasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaComprasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
